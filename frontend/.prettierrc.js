89b4f3e1d7acc3a5afa4336b567cfe1de16ada28
Kolya Bazlov <kolyabazlov@Kolyas-MacBook-Pro.local>
2023-05-04 21:22:20 +0300

    moving front to folder

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    arrowParens: "always",
+    printWidth: 120,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    endOfLine: "auto",
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: "all",
+    overrides: [
+      {
+        files: ["*.js", "*.ts", "*.jsx", "*.tsx"],
+        options: {
+          parser: "typescript",
+        },
+      },
+      {
+        files: ["*.json"],
+        options: {
+          parser: "json",
+        },
+      },
+      {
+        files: ["*.css"],
+        options: {
+          tabWidth: 2,
+        },
+      },
+    ],
+  };
+  
