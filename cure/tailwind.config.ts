bda93117d22b11fec804c4533f875a6de709c21b
Kolya Bazlov <nikolay.tetraform@gmail.com>
2023-09-18 20:33:15 +0400

    cure: feat: added prettify plugins, tailwind config

--- cure/tailwind.config.ts
+++ cure/tailwind.config.ts
@@ -1,20 +1,23 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      sm: '360px',
+      md: '600px',
+      lg: '1200px',
+      xl: '1440px'
     },
-  },
-  plugins: [],
+    colors: {
+      black: '#000000',
+      white: '#FFFFFF'
+    },
+    extend: {
+      zIndex: {
+        100: '100'
+      },
+    }
+  }
 }
 export default config
